cd128b9268bb4baadbca690f5cb78f4dc314b105
Bruce Klein <brucek@hopscotch.to>
2012-07-02 21:52:00 -0700

    New line added to index.php
    "New line added after initial commit!"

--- index.php
+++ index.php
@@ -5,4 +5,5 @@
 <body>
 <p>A trivial website I can use to practice making and reverting changes using GitHub.
 <p>Go to <a href="file2.php">file #2</a>
+<p>New line added after initial commit!  Better commit this to a new change.
 </body></html>
