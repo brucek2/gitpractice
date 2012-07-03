0fb0d7be126ea63669cbffc50fcffe9453e20b38
Bruce Klein <brucek@hopscotch.to>
2012-07-02 22:08:46 -0700

    Added file #3 and a link to it from the home page.

---
+++ file3.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>File 3 - Git Practice Site</title>
+</head>
+<body>
+<p>This is file #3.
+<p><a href="index.php">Return to index</a>
+</body></html>
--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 <body>
 <p>A trivial website I can use to practice making and reverting changes using GitHub.
 <p>Go to <a href="file2.php">file #2</a>
+<p>Go to <a href="file3.php">file #3</a>
 <p>New line added after initial commit!  Better commit this to a new change.
 </body></html>
