07b9ca0a6908beb679945e372c70f72f3da535da
Bruce Klein <brucek@hopscotch.to>
2012-07-02 21:46:58 -0700

    initial commit
    index.php + file2

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Git Practice Site</title>
+</head>
+<body>
+<p>A trivial website I can use to practice making and reverting changes using GitHub.
+<p>Go to <a href="file2.php">file #2</a>
+</body></html>
